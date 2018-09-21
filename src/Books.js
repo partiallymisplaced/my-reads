54740e9a1caabc24b091b1752d99487fe8ff40a8
Nevena Stoeva <nevena.stoeva@gmail.com>
2018-09-21 15:06:31 -0700

    Adds Book component

---
+++ src/Books.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Books extends Component {
+  render() {
+    console.log('Props', this.props);
+    return (
+      <ol className='books-grid'>
+        {this.props.books.map((book) => (
+          <li key={book.title}>
+          <div className="book">
+              <div className="book-top">
+                <img className="book-cover" src={book.url} alt=""/>
+                <div className="book-shelf-changer">
+                  <select>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                  </select>
+                </div>
+              </div>
+              <div className="book-title">{book.title}</div>
+              <div className="book-authors">{book.authors[0]}</div>
+            </div>
+          </li>
+        ))}
+      </ol>
+    )
+  }
+}
+
+export default Books;
